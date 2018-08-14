c2f0d788f8a20101e6ecb8421542a59095a188b4
Kyle <kylesclawson@gmail.com>
2018-08-14 14:21:32 -0600

    Conditional rendering to toggle card view

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -8,13 +8,25 @@ class Product extends Component {
     render() {
         const { item, cardView } = this.props;
         return (
-            <div >
-                <img src={item.imageUrl} alt='the item' className={cardView ? '' : 'product_image'}/>
-                <h4>{item.name}</h4>
-                <p>{item.description}</p>
-                <p>{item.price}</p>
-                <button onClick={() => this.props.addItem(item)}>Add to Cart</button>
-            </div>
+            
+                cardView
+                ?( 
+                    <div>
+                        <h4>{item.name}</h4>
+                        <p>{item.price}</p>
+                        <button onClick={() => this.props.addItem(item)}>Add to Cart</button>
+                    </div>
+                )
+                
+                :(
+                    <div >
+                        <img src={item.imageUrl} alt='the item'/>
+                        <h4>{item.name}</h4>
+                        <p>{item.description}</p>
+                        <p>{item.price}</p>
+                        <button onClick={() => this.props.addItem(item)}>Add to Cart</button>
+                    </div>
+                )
         );
     }
 }

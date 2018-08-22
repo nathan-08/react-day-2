5c648eac95be9554a7390f5ebd4fda299aa34e4f
nathan-08 <nathanryan001@gmail.com>
2018-08-22 14:28:55 -0600

    styling

--- src/App.js
+++ src/App.js
@@ -1,242 +1,227 @@
-import React, { Component } from 'react';
-import './App.css';
-import Product from './Components/Product';
-import CartItem from './Components/CartItem';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      beachGear:[
+      addressInput: "",
+      ccInput: "",
+      cart: [],
+      cardView: true,
+      beachGear: [
         {
-          id:1,
-          name:'Flip Flops',
-          description:'Some flippy floppys',
-          price:5.99,
-          imageUrl:'http://via.placeholder.com/350x150',
-          quantity:0
+          id: 1,
+          name: "Flip Flops",
+          description: "Some flippy floppys",
+          price: 5.99,
+          quantity: 0,
+          imageUrl:
+            "https://i.pinimg.com/736x/86/51/8c/86518c2adfb760bf5e9091841ab6fc9c--girls-flip-flops-beach-flip-flops.jpg"
         },
         {
-          id:2,
-          name:'Tent',
-          description:'TENTS',
-          price:6.99,
-          imageUrl:'http://via.placeholder.com/350x150',
-          quantity:0
-        },
+          id: 3,
+          name: "Sun tan lotion",
+          description: "Gotta look fly guy",
+          price: 7.99,
+          quantity: 0,
+          imageUrl:
+            "https://images.all-free-download.com/images/graphicthumb/summer_cream_protect_lotion_design_vector_582414.jpg"
+        }
       ],
-        camping:[
-          {
-            id:3,
-            name:'Sun tan lotion',
-            description:'Gotta look fly guy',
-            price:7.99,
-            imageUrl:'http://via.placeholder.com/350x150',
-            quantity:0
-          },
-          {
-            id:4,
-            name:'Mice',
-            description:'Not blind',
-            price:8.99,
-            imageUrl:'http://via.placeholder.com/350x150',
-            quantity:0
-          },
-
-        ],
-      
-      cart:[],
-      toggleCard:false,
-      searchFilter:'',
-      toggleCart:false,
-      address:'',
-      creditCard:''
-    }
+      camping: [
+        {
+          id: 2,
+          name: "Tent",
+          description: "TENTS",
+          price: 6.99,
+          quantity: 0,
+          imageUrl:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhEXFLblyNPD1fknwWEaJ-sc_t6o0NANN1ZHltRn6CuEbJUTT0aQ"
+        },
+        {
+          id: 4,
+          name: "Mice",
+          description: "Not blind",
+          price: 8.99,
+          quantity: 0,
+          imageUrl:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Assorted_computer_mice_-_MfK_Bern.jpg/220px-Assorted_computer_mice_-_MfK_Bern.jpg"
+        }
+      ]
+    };
     this.checkout = this.checkout.bind(this);
-    this.handleAddItemToCart = this.handleAddItemToCart.bind(this);
-    this.toggleCardView = this.toggleCardView.bind(this);
-    this.removeItemFromCart = this.removeItemFromCart.bind(this);
-    this.toggleCart = this.toggleCart.bind(this);
-    this.toggleProducts = this.toggleProducts.bind(this);
   }
-  handleAddItemToCart( item ){
-    let newCart = this.state.cart.map( cartItem => {
-      return {
-        id:cartItem.id,
-        name:cartItem.name,
-        description:cartItem.description,
-        price:cartItem.price,
-        imageUrl:cartItem.imageUrl,
-        quantity:cartItem.quantity
-      }
-    })
-    let itemIndex = newCart.findIndex( cartItem => cartItem.id === item.id)
-    if( itemIndex!== -1){
-      newCart[itemIndex].quantity++
+  toggleView = () => this.setState({ cardView: !this.state.cardView });
+  handleAddressInput = event => this.setState({ addressInput: event.target.value });
+  handleCCInput = event => this.setState({ ccInput: event.target.value });
+  deleteFromCart = id => {
+    const { cart } = this.state;
+    let newCart = cart.map(cartItem => Object.assign({}, cartItem));
+    let itemIndex = newCart.findIndex(cartItem => cartItem.id === id);
+    console.log("index: ", newCart);
+    if (newCart[itemIndex].quantity > 1) {
+      newCart[itemIndex].quantity--;
     } else {
-      item.quantity++
-      newCart.push(item)
+      newCart.splice(itemIndex, 1);
+    }
+    this.setState({ cart: newCart });
+  };
+  handleAddItemToCart(item) {
+    const { cart } = this.state;
+    let newCart = cart.map(cartItem => Object.assign({}, cartItem));
+    let match = newCart.find(cartItem => cartItem.id === item.id);
+    if (match) {
+      // item exists on cart
+      match.quantity++;
+    } else {
+      item.quantity++;
+      newCart.push(item);
     }
     this.setState({
-      cart:newCart
-    })
+      cart: newCart
+    });
   }
-
-  removeItemFromCart( id ){
-    let newCart = this.state.cart.map( cartItem => {
-      return {
-        id:cartItem.id,
-        name:cartItem.name,
-        description:cartItem.description,
-        price:cartItem.price,
-        imageUrl:cartItem.imageUrl,
-        quantity:cartItem.quantity
-      }
-    })
-    let itemIndex = newCart.findIndex( cartItem => cartItem.id === id)
-    if(newCart[itemIndex].quantity === 1){
-      newCart.splice(itemIndex,1)
-    }
-    else {
-      newCart[itemIndex].quantity--
-    }
-    this.setState({
-      cart:newCart
-    })
-  }
-
-  checkout(){
-    if(!this.state.address || !this.state.creditCard){
-      alert("WHERE YOU LIVE. WHERE MY MONEY")
-    }
-    else{
-      alert("Here's yer stuff")
+  checkout() {
+    if (!this.state.addressInput || !this.state.ccInput) {
+      alert("Fill out required fields first.");
+    } else if (this.state.cart.length === 0) {
+      alert("cart is empty.");
+    } else {
+      alert("Here's yer stuff");
       this.setState({
-        cart:[]
-      })
+        cart: [],
+        addressInput: "",
+        ccInput: ""
+      });
     }
-  }
-  toggleCardView(){
-    this.setState({
-      toggleCard:!this.state.toggleCard
-    })
-  }
-  search( searchInput ){
-    this.setState({
-      searchFilter:searchInput
-    })
-  }
-  toggleCart(){
-    this.setState({
-      toggleCart:true
-    })
-  }
-  toggleProducts(){
-    this.setState({
-      toggleCart:false
-    })
-  }
-  handleAddressInput( address ){
-    this.setState({
-      address
-    })
-  }
-  handleCreditCardInput( creditCard ){
-    this.setState({
-      creditCard
-    })
   }
   render() {
     return (
       <div>
-        <nav>
-          <button onClick={this.toggleCardView}>Toggle View</button>
-          <button onClick={this.toggleProducts}>See Products</button>
-          <button onClick={this.toggleCart}>See Cart</button>
-        </nav>
-        {
-
-        !this.state.toggleCart
-        ?<div className='products'>
-          <h1>PRODUCTS</h1>
-          
-          <p>Search Product Names: </p>
-          <input value={this.state.searchFilter} onChange={ e => this.search(e.target.value)}/>
-          <h2>Beach Gear</h2>
-          {
-            this.state.beachGear.filter( item => item.name.includes(this.state.searchFilter))
-            .map( item => {
-              return(
-                // <div>
-                //   <img src={item.imageUrl} alt='the item'/>
-                //   <h4>{item.name}</h4>
-                //   <p>{item.description}</p>
-                //   <p>{item.price}</p>
-                //   <button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button>
-                // </div>
-                <Product
-                  item={item}
-                  addItem={this.handleAddItemToCart}
-                  cardView={this.state.toggleCard}
-                />
-              )
-            })
-          }
-          <h2>Camping</h2>
-          {
-            this.state.camping.filter( item => item.name.includes(this.state.searchFilter))
-            .map( item => {
-              return(
-                // <div>
-                //   <img src={item.imageUrl} alt='the item'/>
-                //   <h4>{item.name}</h4>
-                //   <p>{item.description}</p>
-                //   <p>{item.price}</p>
-                //   <button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button>
-                // </div>
-                <Product
-                  item={item}
-                  addItem={this.handleAddItemToCart}
-                  cardView={this.state.toggleCard}
-                />
-              )
-            })
-          }
-          
-        </div>
-        
-        :<div className='side_bar'>
-          <div className='cart'>
+        <section className="products">
+          <div className="products_header">
+            <h1>PRODUCTS</h1>
+            <button onClick={this.toggleView}>toggle view</button>
+          </div>
+          <table className="products_body">
+            <thead>
+              <th colspan="2">
+                <h2>Beach Gear</h2>
+              </th>
+            </thead>
+            {this.state.beachGear.map(item => {
+              if (this.state.cardView)
+                return (
+                  <tr className="product_container clearfix">
+                    <td>
+                      <img className="product_img" src={item.imageUrl} />
+                    </td>
+                    <td>
+                      <h4>{item.name}</h4>
+                      <p>{item.description}</p>
+                      <p>{item.price}</p>
+                      </td><td>
+                      <button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button>
+                    </td>
+                  </tr>
+                );
+              else
+                return (
+                  <tr className="product_container clearfix">
+                    <td>
+                      <h4>{item.name}</h4>
+                    </td>
+                    <td>{item.description}</td>
+                    <td>{item.price}</td>
+                    <td><button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button></td>
+                  </tr>
+                );
+            })}
+            <thead><th colspan="2">
+            <h2>Camping</h2></th>
+            </thead>
+            {this.state.camping.map(item => {
+              if (this.state.cardView)
+                return (
+                  <tr className="products_container clearfix">
+                    <td>
+                      <img className="product_img" src={item.imageUrl} />
+                    </td>
+                    <td>
+                      <h4>{item.name}</h4>
+                      <p>{item.description}</p>
+                      <p>{item.price}</p></td><td>
+                      <button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button>
+                    </td>
+                  </tr>
+                );
+              else
+                return (
+                  <tr className="product_container clearfix">
+                    <td>
+                      <h4>{item.name}</h4>
+                    </td>
+                    <td>{item.description}</td>
+                    <td>{item.price}</td>
+                    <td><button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button></td>
+                  </tr>
+                );
+            })}
+          </table>
+        </section>
+        <section className="cart">
+          <div className="cart_header">
             <h1>CART</h1>
-            {
-              this.state.cart.map( item => {
-                return( 
-                  // <div>
-                  //   <h4>{item.name}</h4>
-                  //   <p>{item.price}</p>
-                  // </div>
-                  <CartItem
-                    item={item}
-                    removeItem={this.removeItemFromCart}
-                  />
-                )
-              })
-            }
-
+            <div className="total">
+              <table>
+                <tr>
+                  <td>
+                    <label>address</label>
+                  </td>
+                  <td>
+                    <input type="text" value={this.state.addressInput} onChange={this.handleAddressInput} />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <label>credit card number</label>
+                  </td>
+                  <td>
+                    <input type="text" value={this.state.ccInput} onChange={this.handleCCInput} />
+                  </td>
+                </tr>
+              </table>
+              <h4>TOTAL</h4>
+              <p>
+                $
+                {this.state.cart
+                  .reduce((accumulator, current) => (accumulator += current.price * current.quantity), 0)
+                  .toFixed(2)}
+              </p>
+              <button onClick={this.checkout}>Checkout</button>
+            </div>
           </div>
-          <div className='inputs'>
-            <input placeholder='address' value={this.state.address} onChange={ (e) => this.handleAddressInput(e.target.value)}/>
-            <input placeholder='credit card info' value={this.state.creditCard} onChange={ (e) => this.handleCreditCardInput(e.target.value)}/>
-          </div>
-          <div className='total'>
-            <h1>TOTAL</h1>
-            <p>${
-              this.state.cart.reduce( ( accumulator, current ) => accumulator+= current.price*current.quantity,0)
-            }</p>
-            <button onClick={this.checkout}>Checkout</button>
-          </div>
-        </div>
-        }
+          <table className="cart_body">
+            {this.state.cart.map(item => {
+              return (
+                <tr className="products_container clearfix">
+                  <td>
+                    <img className="product_img" src={item.imageUrl} />
+                  </td>
+                  <td>
+                    <h4>{item.name}</h4>
+                    <span>{(item.price * item.quantity).toFixed(2)}</span> <span> | qty: </span>
+                    <span>{item.quantity}</span>
+                    <br />
+                    <button onClick={_ => this.deleteFromCart(item.id)}>remove item</button>
+                  </td>
+                  <td />
+                </tr>
+              );
+            })}
+          </table>
+        </section>
       </div>
     );
   }
@@ -244,11 +229,19 @@ class App extends Component {
 
 export default App;
 
-// Day 2 starting code starts at day 1's stage 2 solution
-
-// Day2 props
-// proptypes, reusable components, functional component props
-// 1 - List into seperate components, include add to cart functionality
-// 2- Add proptypes onto a list item, Reusable text component, based on props changes styling: header, subtext, list view vs card view required.
-// 3 - ?
-// 4- Add search filter functionality, onchange or onclick, add navbar buttons at the top that toggle between card and product view. Fake if/else conditional rendering. 1 or 2 opportunitiesOther opportunites that text is a functional component. Improve input validation. changing quantities to 0, removes from cart etc.
+// 1 - single screen, sidebar, left side is a list of products.
+// 2 - add in props - list component, list item component, clean it up
+// 3 - add in axios.
+// Each day every project has 4 stages
+// day 1
+// - 1 mini project + 20-30% more
+//     - class, functional, state
+//     - list of products, pre-hardcoded onto state. Click add to cart and it adds it to a list on the sidebar. Clicking add to cart multiple times increases the qty.
+// - 2 is where the afternoon project is now
+//     - get a total, left side, multiple categories. Header for each category that is not seperate component, different background color or header color for each category. Checkout btn, cart is cleared. No nesting state.Each category is a seperate property on state
+// - 3 including concepts from previous days: never super complex
+//     - user input validation
+//     - fake credit card payment
+// - 4 black diamond - almost no direction
+//     - Delete item out of cart, update quantity, update between list and card view on product side. Idea is conditional rendering.
+// Stage 3-4 need to stay independant of other projects. These are the push yourselves levels.
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -3,45 +3,41 @@ import PropTypes from "prop-types";
 import "./../App.css";
 import Text from "./Text";
 
-class Product extends Component {
-  render() {
-    const { item, cardView } = this.props;
-    return cardView ? (
-      <div>
-        
-        <Text 
-            text={item.name} 
-            isHeader={true}
-        />
-        <Text
-            text={'$' + item.price}
-            isHeader={false}
-        />
-        <button onClick={() => this.props.addItem(item)}>Add to Cart</button>
-      </div>
-    ) : (
-      <div>
-        <img src={item.imageUrl} alt="the item" />
-        <Text
-            text={item.name}
-            isHeader={true}
-        />
-        <Text
-            text={item.description}
-            isHeader={false}
-        />
-        <Text
-            text={'$' + item.price}
-            isHeader={false}
-        />
-        <button onClick={() => this.props.addItem(item)}>Add to Cart</button>
-      </div>
-    );
-  }
+function Product (props) {
+    const { item, cardView, addItem } = this.props;
+    if (props.cardView)
+                return (
+                  <tr className="products_container clearfix">
+                    <td>
+                      <img className="product_img" src={item.imageUrl} />
+                    </td>
+                    <td>
+                      <h4>{item.name}</h4>
+                      <p>{item.description}</p>
+                      <p>{item.price}</p></td><td>
+                      <button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button>
+                    </td>
+                  </tr>
+                );
+              else
+                return (
+                  <tr className="product_container clearfix">
+                    <td>
+                      <h4>{item.name}</h4>
+                    </td>
+                    <td>{item.description}</td>
+                    <td>{item.price}</td>
+                    <td><button onClick={() => this.handleAddItemToCart(item)}>Add to Cart</button></td>
+                  </tr>
+                );
 }
-Product.propTypes = {
-  item: PropTypes.object.isRequired,
-  addItem: PropTypes.func.isRequired,
-  cardView: PropTypes.bool
-};
-export default Product;
+
+
+
+// Step 2, proptypes
+// Product.propTypes = {
+//   item: PropTypes.object.isRequired,
+//   addItem: PropTypes.func.isRequired,
+//   cardView: PropTypes.bool
+// };
+// export default Product;
